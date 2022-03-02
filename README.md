40623c93e0b5db6254803036aef5e21df69be860
head-bro <milikmichal@gmail.com>
2022-03-02 20:56:58 +0100

    update for lesson 1

--- README.md
+++ README.md
@@ -2,8 +2,22 @@
 First steps in terraform environment.
 
 # Lesson 1
-- provision EC2 instance (t2.micro) in eu-central-1
-- "terraform init" to init provider plugins (AWS)
-- "terraform plan" to check what will be done/provision
-- "terraform apply" to run code
- 
+## provision EC2 instance (t2.micro) in eu-central-1
+* "terraform init" to init provider plugins (AWS)
+* "terraform plan" to check what will be done/provision
+* "terraform apply" to run code
+## add 'hello world'
+* add aws security group to open 8080 port
+* add security group to ec2 vcp_security_group_ids
+* variables and how to provide it:
+    * environment var
+    ```bash
+    export TF_VAR_{name_of_variable}=8080
+    ```
+    * default value
+    * if no default value than terafform will promt for it after command 'plan' or 'apply'
+    * as a parameter with command 'plan' or 'apply':
+    ```bash
+    terraform plan -var "name_of_var=8080"
+    terraform apply -var "name_of_var=8080"
+    ``` 
--- lesson_1/main.tf
+++ lesson_1/main.tf
@@ -5,8 +5,45 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0a932bbdde8ed7b5f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup python -m http.server ${var.server_port} &
+              EOF
 
   tags = {
     "Name" = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = "terreform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
+}
+
+variable "server_port" {
+  description = "port number for web page"
+  type = number
+  default = 8080
 }
